374ac7f1017929a86a87ae6bc2e843dcf6297496
jasme <jasme@10.48.51.247>
2024-01-24 10:21:04 -0500

    added a part time student class

---
+++ src/arraydemo/partTimestudent.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package arraydemo;
+
+/**
+ *
+ * @author jasme
+ */
+public class partTimestudent extends Student {
+    
+        private int numOfCourses;
+
+        public int getNumOfCourses() {
+            return numOfCourses;
+        }
+
+        public void setNumOfCourses(int numOfCourses) {
+            this.numOfCourses = numOfCourses;
+        }
+
+        public partTimestudent(int numOfCourses) {
+            this.numOfCourses = numOfCourses;
+        }
+        
+    }
+    
+
