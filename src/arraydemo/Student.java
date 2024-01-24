1b8f297231c32512c8e0361bd353c47aa8a25c60
jasme <jasme@10.48.51.247>
2024-01-24 09:45:07 -0500

    made some changes

--- src/arraydemo/Student.java
+++ src/arraydemo/Student.java
@@ -2,9 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Arraydemo;
+package arraydemo;
+
 /**
- *hello this has been edited from remote
+ *
  * @author jasme
  */
 public class Student {
@@ -20,3 +21,5 @@ public class Student {
     }
     
 }
+
+
