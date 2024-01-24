36cf89ac50fd6fcc1642ce910bb466c5970e634a
jasme <jasme@10.48.51.247>
2024-01-24 09:17:00 -0500

    first comment

---
+++ src/Student.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Arraydemo;
+/**
+ *
+ * @author jasme
+ */
+public class Student {
+    private String sname;
+    private String Address;
+    private String getAddress(){
+        return Address;
+        
+    }
+    
+    public void setAddress (String Address) {
+        this.Address = Address ;
+    }
+    
+}
